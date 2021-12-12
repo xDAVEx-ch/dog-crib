849f8af57b007237bdbb3336cde6511ec60406d5
Dave Ch <dabbidchavarria@gmail.com>
2021-12-12 02:03:07 -0600

    Initial configuration

---
+++ theme/default/colors.js
@@ -0,0 +1,16 @@
+export const colors = {
+  bg: {
+    light: '#ffffff',
+    dark: '#1a535c',
+  },
+  iu: {
+    primary: '#4ecdc4',
+    secondary: '#1A535C',
+    error: '#FF6B6B',
+    accent: '#FFE66D',
+  },
+  text: {
+    primary: '#111111',
+    secondary: '#ffffff',
+  },
+};
